154869b1dae3c96f99fb4f0718eea0c7a2644783
Emiliyss <90375230+Emiliyss@users.noreply.github.com>
2021-10-25 19:33:30 +0300

    Update PU2-dectobin.c

--- Mape/PU2-dectobin.c
+++ Mape/PU2-dectobin.c
@@ -3,13 +3,12 @@
 int main() {
 	printf("Ievadiet skaitli līdz 255\n ");
 	printf("Jūsu skaitļa binārais kods: ");
-	unsigned int sk;
+	unsigned int sk,i;
 	int bin[8];
     scanf("%d", &sk);
-	for (int i = 0; i < 8; i++) {
+	for (i = 0; i < 8; i++) {
 	if (sk%2 != 0) {bin[i]=1; sk = sk-->>1;} else {bin[i]=0; sk = sk>>1;}
 	printf("%d", bin[i]);
 	}
-  
 	return 0;
 }
