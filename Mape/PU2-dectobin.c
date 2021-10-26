85d03f8ed91c09f5343b0a875a4ca6ce5db84713
Emiliyss <90375230+Emiliyss@users.noreply.github.com>
2021-10-26 12:23:07 +0300

    Update PU2-dectobin.c

--- Mape/PU2-dectobin.c
+++ Mape/PU2-dectobin.c
@@ -1,12 +1,17 @@
-#include <stdio.h>
+//Ne ļoti racionāli, bet pēc uzdevuma nosacījuma viss ir izpildīts. Nu, vismaz darbs ir godprātīgi izstrādāt, nenorakstīts no interneta. Ar funkciju ATOI iepazīstinājos internetā
+//es saprotu, ka vajadzēja pildīt pavisam citādi, bet pagaidām pareizo variantu nesaprotu
+//mainīgais Z bija izveidots, lai pārrakstītu bināro kodu pareizajā virzienā, līdzi šim 1(dec) izskatījās kā 10000000(bin)
 
+#include <stdio.h>
+#include <stdlib.h>
 int main() {
 	printf("Ievadiet skaitli līdz 255\n ");
 	printf("Jūsu skaitļa binārais kods: ");
 	unsigned int sk,i,z;
 	z = 1;
-	int bin[8];
-    scanf("%d", &sk);
+	char bin[8];
+	scanf("%s", &bin);
+	sk = atoi(bin);
 	for (i = 0; i < 16; i++) {
 	if (i>7) { printf("%d", bin[i-z]); z +=2 ; continue;};
 	if (sk%2 != 0) {bin[i]=1; sk = sk-->>1;} else {bin[i]=0; sk = sk>>1;}
